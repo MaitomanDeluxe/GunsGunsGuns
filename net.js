98b699a04e3047517a7536166250adca8b518a55
MaitomanDeluxe <maikanamaikana@outlook.jp>
2025-07-09 19:32:41 +0900

    Update net.js

--- net.js
+++ net.js
@@ -1,16 +1,30 @@
 let socket;
+let myId = null;
+let players = {};
+let bullets = [];
 
 function send(msg) {
-  socket.send(JSON.stringify(msg));
+  if (socket && socket.readyState === WebSocket.OPEN) {
+    socket.send(JSON.stringify(msg));
+  }
 }
 
 function startSocket() {
-  // net.js の該当行
-  const socket = new WebSocket("wss://gunsgunsguns.maikanamaikana.workers.dev/");
+  socket = new WebSocket("wss://gunsgunsguns.maikanamaikana.workers.dev/");
 
-
+  socket.onopen = () => {
+    console.log("WebSocket connected");
+  };
+  socket.onerror = (e) => {
+    console.error("WebSocket error", e);
+  };
+  socket.onclose = () => {
+    console.log("WebSocket closed");
+  };
   socket.onmessage = (event) => {
     const msg = JSON.parse(event.data);
+    console.log("[WS受信]", msg);
+
     if (msg.type === "init") {
       myId = msg.id;
     } else if (msg.type === "players") {
